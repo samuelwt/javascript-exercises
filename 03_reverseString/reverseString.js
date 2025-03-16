c8b7e7d4720290cd77a147f23c333c82df7a9af6
samuelwt <samwinson908@gmail.com>
2025-03-15 21:38:39 -0400

    reverse string using arrays

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    const charArray = inputString.split(""); // splits string to characters in an array
+    charArray.reverse(); // reverses elements of the array
+    return charArray.join(""); // joins and converts to string
 };
 
 // Do not edit below this line
