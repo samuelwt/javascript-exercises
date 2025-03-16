f176f4459bf8c149fa6e58d87df799c6c18bbf89
samuelwt <samwinson908@gmail.com>
2025-03-15 21:30:19 -0400

    simple for loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
-
+const repeatString = function(string, num_times) {
+    // num_times must be a positive, integer
+    // maybe check if string is of type string
+    if (num_times < 0) {
+        console.log("Please key in a non-negative integer.");
+        return "ERROR";
+    } else {
+        num_times = Math.floor(num_times);
+        let outputString = string;
+        if (num_times == 0) {
+            return "";
+        } else {
+            for (let i=0; i < num_times-1; i++) {
+                outputString += string;
+            }
+            return outputString;
+        };
+    };
 };
 
 // Do not edit below this line
