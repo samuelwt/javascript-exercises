625e8d433779ee64eb8dd02e0a82f7c922354592
samuelwt <samwinson908@gmail.com>
2025-03-15 22:23:27 -0400

    rest parameters, loops, and arrays

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...arrayElem) {
+    // the rest parameter takes in an uninitialized number of arguments as an array
+    let index;
+    for (let elem of arrayElem) {
+        index = 0;
+        while (index !== -1) {
+            index = array.indexOf(elem);
+            if (index == -1) continue;
+            array.splice(index,1);
+            index = array.indexOf(elem); // check masih ada ga?
+        };
+    };
+    return array;
 };
 
 // Do not edit below this line
