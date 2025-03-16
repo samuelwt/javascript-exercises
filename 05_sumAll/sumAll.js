de63d89befdeb7ed48a8acb5934d72b60e56b257
samuelwt <samwinson908@gmail.com>
2025-03-15 22:40:09 -0400

    sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(num_start, num_end) {
+    if ((Number.isInteger(num_start) != true) || (Number.isInteger(num_end) != true)) {
+        return "ERROR";
+    } else if ((num_start < 0) || (num_end < 0)) {
+        return "ERROR";
+    } else {
+        let large_num;
+        let small_num;
+        if (num_start > num_end) {
+            large_num = num_start;
+            small_num = num_end;
+        } else {
+            large_num = num_end;
+            small_num = num_start;
+        };
+        let sum = 0;
+        for (let i = small_num; i < large_num+1; i++) {
+            sum += i;
+        };
+        return sum;
+    };
 };
 
 // Do not edit below this line
